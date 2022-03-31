4910fa0add53f903e5afdc34bb29b23e59e5a61c
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-03-31 14:45:23 +0200

    created functions to display numbers

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
+// variables
+const equalsButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
+const operatorButton = document.getElementsByClassName("operator");
 const operandButton = document.getElementsByClassName("operand");
 const display = document.getElementById("display");
-let displayValue = 0;
-display.innerHTML = displayValue;
-// console.log(operandButton);
+let displayValue;
+let operator;
+let solution;
+
+// The click event to clear the display
+clearButton.addEventListener("click", clearButtonIsClicked);
+
+// The click event to display the numbers
 for (let i = 0; i < operandButton.length; i++) {
-  operandButton[i].addEventListener("click", isClicked);
-}
-function isClicked() {
-  console.log("button is clicked!");
+  operandButton[i].addEventListener("click", operandButtonIsClicked);
 }
 
+// The click event to return an operator
+for (let i = 0; i < operatorButton.length; i++) {
+  operatorButton[i].addEventListener("click", operatorButtonIsClicked);
+}
+
+// The function that returns and displays the numbers
+function operandButtonIsClicked(e) {
+  displayValue = display.textContent += e.target.value;
+  if (displayValue.length === 11) {
+    for (let i = 0; i < operandButton.length; i++) {
+      operandButton[i].disabled = true;
+    }
+  }
+  return displayValue;
+}
+
+// The function that clears the numbers from the display
+function clearButtonIsClicked() {
+  display.textContent = "";
+}
+
+// The function that returns an operator
+function operatorButtonIsClicked(e) {
+  return (operator = e.target.value);
+}
+
+// operation functions
 function add(a, b) {
   return a + b;
 }
